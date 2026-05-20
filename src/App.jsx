17ede25802ebec195b35ec69d3290b69680c155e
Vera Krumpan <filimonovavera96@gmail.com>
2026-05-20 19:21:58 +0300

    feat: add onClose and isOpen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import Modal from "./Modal/Modal";
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <>
-      <Modal title="Подтверждение действия">
+      <button onClick={() => setOpenModal(true)}>Open Modal</button>
+      <Modal
+        isOpen={openModal}
+        onClose={() => setOpenModal(false)}
+        title="Подтверждение действия"
+      >
         <p>Вы уверены, что хотите удалить файл?</p>
         <button>Да</button>
         <button>Нет</button>
--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,10 +1,36 @@
 import "./Modal.css";
 
-const Modal = ({ title, children }) => {
+const Modal = ({ isOpen, onClose, title, children }) => {
+  if (!isOpen) return null;
+
   return (
-    <div className="modal">
-      <div className="modal-backdrop">
-        <div className="modal-content">
+    <div className="modal" onClick={onClose}>
+      <div className="modal-backdrop" >
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <button className="close-btn" onClick={onClose}>
+            <svg
+              width="20px"
+              height="20px"
+              viewBox="-0.5 0 25 25"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3 21.32L21 3.32001"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M3 3.32001L21 21.32"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
           <div className="modal-header">
             <h2>{title}</h2>
           </div>
@@ -16,3 +42,9 @@ const Modal = ({ title, children }) => {
 };
 
 export default Modal;
+
+// onKeyDown={(e) => {
+//         if (e.key === "Escape") {
+//           onClose();
+//         }
+//       }}
